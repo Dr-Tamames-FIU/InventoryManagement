81aa60506e111bde8c6e2bf3b34705f55fac45e4
Luisana Barajas <luisana.barajas@gmail.com>
2024-03-29 16:45:44 -0400

    first attend

---
+++ Product.java
@@ -0,0 +1,26 @@
+public class Product {
+
+    /* Declare attributes for the products
+     * 
+     * scanningNumber is an integer
+     * name is a string
+     * quantity is an integer
+     */
+
+     //Constructor for product attributes 
+
+     //Methods to get and set attributes
+
+     //Method to scan number of the product and return an integer
+
+     //Method to set scanning number of product
+
+     //method to get name of product
+
+     //method to set name of product
+
+     //method to get quantity of product in stock
+
+     //method to set quantity of product in stock 
+    
+}
